7798d85f39e2953e040e5a20b386e50311b84b42
Rares Jianu <Rares.Jianu@endava.com>
2022-02-12 12:58:01 +0200

    We need migrations to create the table inside the database

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,14 +1,29 @@
-import { Ctx, Query, Resolver } from "type-graphql";
+import { Ctx, Query, Resolver, Int, Arg, Mutation } from "type-graphql";
 import { Post } from "../entities/Post";
 import { MyContext } from "../types";
 
 @Resolver()
 export class PostResolver {
     @Query(() => [Post])
-    posts(
-        @Ctx() ctx: MyContext
-    ) {
+    posts(@Ctx() {em}: MyContext): Promise<Post[]> {
+        return em.find(Post, {})
+    }
 
-        return "Hello world"
+    @Query(() => Post, {nullable: true})
+    post(
+        @Arg("id", () => Int) id: number,
+        @Ctx() {em}: MyContext
+        ): Promise<Post | null> {
+        return em.findOne(Post, {id})
+    }
+
+    @Mutation(() => Post)
+    async createPost(
+        @Arg("title", () => String) title: string,
+        @Ctx() {em}: MyContext
+        ): Promise<Post> {
+            const post = em.create(Post, {title, createdAt: new Date(), updatedAt: new Date()})
+            await em.persistAndFlush(post)
+        return post;
     }
 }
